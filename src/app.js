ddbed96be8adf356bf0444185f3d0344d30c3d6c
Navaneeth Arunkumar <navaarun18@gmail.com>
2022-12-12 15:44:10 +0530

    Lab

--- src/app.js
+++ src/app.js
@@ -23,28 +23,47 @@ const PLAYERS = [
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
-
     // Instead of forloop use Map method
     // Code here
-
+    detailedPlayers= players.map((pName, i) => {
+        return{
+            name:pName,
+            strength:getRandomStrength(),
+            image:`images/super-${i+1}.png`,
+            type: i%2==0?"hero":"villain"
+        }
+    })
     return detailedPlayers;
 }
+
 
 // getting random strength
 const getRandomStrength = () => {
     return Math.ceil(Math.random() * 100);
 }
 
+
+
 // Build player template
 const buildPlayers = (players, type) => {
-    let fragment = '';
-
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
-
-    return fragment;
+    let valid=players.filter(playerData=>{
+        if (playerData.type == type) {
+            return true;
+        }
+    })
+    let fragment=valid.map((playerData) => {
+        return `
+        <div class="player">
+            <img src="${playerData.image}" alt="">
+            <div class="name">${playerData.name}</div>
+            <div class="strength">${playerData.strength}</div>    
+        </div>
+        `;
+    });
+    return fragment.join("");
 }
 
 // Display players in HTML
